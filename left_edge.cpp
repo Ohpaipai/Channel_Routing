c88dc104d6d44169731b80188d4592caa0e31dbc
soloman <zx40904@gmail.com>
2019-12-19 21:35:04 +0800

    improve input

--- left_edge.cpp
+++ left_edge.cpp
@@ -4,16 +4,20 @@
 #include<string>
 #include<map>
 #include<sstream>
-int readFile(){ //return pcb long
+int readFile(std::map<std::string,std::pair<int,int>>&biglong ,std::vector<std::string>&top ,std::vector<std::string>&tail){ //return pcb long
 	int PCBcount =0;
 	std::fstream infile;
 	infile.open("test.pin",std::ios::in);
 	std::string name;//name
 	std::string line;
-	std::map<std::string,std::vector<int>>topPCB;//top PCB
-	std::map<std::string,std::vector<int>>underPCB;//underlayer
+	
+	std::map<std::string,std::pair<int,int>>::iterator it;//iterator
+	
+	//stringstream
 	std::stringstream ss;
 	std::stringstream files;
+
+	//first chaneel
 	std::getline(infile,line);
 		files.str(line);
 		ss.clear();
@@ -22,9 +26,27 @@ int readFile(){ //return pcb long
 			ss<<name;
 		    int placenum;
 			ss>>placenum;	   
-			topPCB[name].push_back(placenum);	
+			top.push_back(name);	
 			PCBcount++;
+			//find
+			it=biglong.find(name);
+			if(it!=biglong.end()){
+				if(it->second.first>placenum){
+					it->second.first=placenum;
+				}
+				else if(it->second.second<placenum){
+					it->second.second=placenum;
+				}
+			}
+			else{
+				std::pair<int,int>tem;
+				tem.first=placenum;
+				tem.second=placenum;
+				biglong[name]=tem;
+			}
+
 		}
+	//second channel
 	std::getline(infile,line);
 		files.clear();
 		files.str(line);
@@ -34,13 +56,44 @@ int readFile(){ //return pcb long
 			ss<<name;
 		    int placenum;
 			ss>>placenum;	   
-			underPCB[name].push_back(placenum);	
+			tail.push_back(name);	
+			it=biglong.find(name);
+			if(it!=biglong.end()){
+				if(it->second.first>placenum){
+					it->second.first=placenum;
+				}
+				else if(it->second.second<placenum){
+					it->second.second=placenum;
+				}
+			}
+			else{
+				std::pair<int,int>tem;
+				tem.first=placenum;
+				tem.second=placenum;
+				biglong[name]=tem;
+			}
 		}	
 	
 	return PCBcount;
 }
+
 int main(int argc, char *argv[]){
-	std::cout<<readFile()<<std::endl;
+	std::vector<std::string>top;//top  channel
+	std::vector<std::string>tail;//tail chanel
+	std::map<std::string,std::pair<int,int>>biglong;//node between longgest
+	std::cout<<readFile(biglong,top,tail)<<std::endl;
+	for(int i=0;i<top.size();i++){
+		std::cout<<top[i]<<" ";
+	}
+	std::cout<<std::endl;
+	for(int i=0;i<top.size();i++){
+		std::cout<<tail[i]<<" ";
+	}
+	std::cout<<std::endl;
+	std::map<std::string,std::pair<int,int>>::iterator it;//iterator
+	for(it=biglong.begin();it!=biglong.end();it++){
+		std::cout<<it->first<<"->"<<it->second.first<<"~"<<it->second.second<<std::endl;
+	}
 	return 0;
 	
 }
