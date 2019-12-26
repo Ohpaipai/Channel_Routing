16fb015e6726a4af29ac5be8b14adda23d658043
soloman <zx40904@gmail.com>
2019-12-26 13:05:28 +0800

    scale
    scale Retectangle database need to change to gdouble

--- gui.h
+++ gui.h
@@ -29,18 +29,28 @@ bool operator<(const Node& s1, const Node& s2)
 {
  	  return s1.begin < s2.begin;
 }
+typedef struct {
+	gdouble x;
+	gdouble y;
+	gdouble width;
+	gdouble height;
+}MyRectangle;
 /*my global parameter*/
 int firstx;//初始x座標
 int firsty;//初始y座標
 int fontsize;//pin腳名font size
-std::vector<std::pair<GdkRectangle,std::string>>topleg;//上層pin腳 leg位置
-std::vector<std::pair<GdkRectangle,std::string>>tailleg;//下層pin腳 leg位置
+std::vector<std::pair<MyRectangle,std::string>>topleg;//上層pin腳 leg位置
+std::vector<std::pair<MyRectangle,std::string>>tailleg;//下層pin腳 leg位置
 std::vector<Windownode>topwnode;//上層pin腳位置
 std::vector<Windownode>tailwnode;//下層pin腳位置
 std::map<std::string,Windownode>outnode;//track x座標位置
 std::vector<pinnum>topPnum;//top pin腳名
 std::vector<pinnum>tailPnum;//tail pin腳名
 int screenshotcount=0;
+gint limitwindowx=0;
+gint limitwindowy=0;
+double scalesize=1;
+GtkWidget *window ;
 /*__________________*/
 static gboolean on_expose_event(GtkWidget *widget, GdkEventExpose *event, gpointer data)
 {
@@ -63,8 +73,18 @@ static gboolean on_expose_event(GtkWidget *widget, GdkEventExpose *event, gpoint
     static gint bigy = 200;
     static gint delta = 1;
 for (int i=0;i<topwnode.size();i++){
-	if(topwnode[i].rec.x<width&&topwnode[i].rec.y<height){
+	if(limitwindowx<=topwnode[i].rec.x<(width+limitwindowx) && limitwindowy<=topwnode[i].rec.y<(height+limitwindowy)){
 		 
+		cairo_set_source_rgb(cr,0,255,179);
+		cairo_rectangle(cr,topwnode[i].rec.x,topwnode[i].rec.y,topwnode[i].rec.width,topwnode[i].rec.height);
+		cairo_fill(cr);
+		cairo_set_source_rgb(cr,0,0,0);
+		cairo_set_font_size(cr,fontsize);
+		cairo_move_to(cr,topPnum[i].x,topPnum[i].y);
+		cairo_show_text(cr,topPnum[i].name.c_str());	
+	}
+	else if(limitwindowx<=topwnode[i].rec.x+topwnode[i].rec.width<(width+limitwindowx) && limitwindowy<=topwnode[i].rec.y<(height+limitwindowy)){
+			
 		cairo_set_source_rgb(cr,0,255,179);
 		cairo_rectangle(cr,topwnode[i].rec.x,topwnode[i].rec.y,topwnode[i].rec.width,topwnode[i].rec.height);
 		cairo_fill(cr);
@@ -78,7 +98,17 @@ for (int i=0;i<topwnode.size();i++){
 	}
 }
 for (int i=0;i<topwnode.size();i++){
-	if(tailwnode[i].rec.x<width&&tailwnode[i].rec.y<height){
+	if(limitwindowx<=tailwnode[i].rec.x<width+limitwindowx&&limitwindowy<=tailwnode[i].rec.y<height+limitwindowy){
+		cairo_set_source_rgb(cr,0,255,179);
+		cairo_rectangle(cr,tailwnode[i].rec.x,tailwnode[i].rec.y,tailwnode[i].rec.width,tailwnode[i].rec.height);
+		cairo_fill(cr);
+		cairo_set_source_rgb(cr,0,0,0);
+		cairo_set_font_size(cr,fontsize);
+		cairo_move_to(cr,tailPnum[i].x,tailPnum[i].y);
+		cairo_show_text(cr,tailPnum[i].name.c_str());	
+	}
+	else if(limitwindowx<=tailwnode[i].rec.x+tailwnode[i].rec.width<(width+limitwindowx) && limitwindowy<=tailwnode[i].rec.y<(height+limitwindowy)){
+		
 		cairo_set_source_rgb(cr,0,255,179);
 		cairo_rectangle(cr,tailwnode[i].rec.x,tailwnode[i].rec.y,tailwnode[i].rec.width,tailwnode[i].rec.height);
 		cairo_fill(cr);
@@ -96,29 +126,42 @@ std::map<std::string,Windownode>::iterator ittrack;
 for(ittrack = outnode.begin(); ittrack != outnode.end(); ittrack++){
 	//std::cout <<ittrack->first<<std::endl;	
 	if(ittrack->first =="0") continue;
-	if(ittrack->second.rec.x<width&&ittrack->second.rec.y<height){
+	if(limitwindowx<=ittrack->second.rec.x<width+limitwindowx&&limitwindowy<=ittrack->second.rec.y<height+limitwindowy){
 		cairo_set_source_rgb(cr,255,0,0);
 		cairo_rectangle(cr,ittrack->second.rec.x,ittrack->second.rec.y,ittrack->second.rec.width,ittrack->second.rec.height);
-//		std::cout<<ittrack->second.rec.x<<" "<<ittrack->second.rec.y<<std::endl;
 		cairo_fill(cr);
-	
+	}
+	else if(limitwindowx<=ittrack->second.rec.x+ittrack->second.rec.width<width+limitwindowx&&limitwindowy<=ittrack->second.rec.y<height+limitwindowy){
+		cairo_set_source_rgb(cr,255,0,0);
+		cairo_rectangle(cr,ittrack->second.rec.x,ittrack->second.rec.y,ittrack->second.rec.width,ittrack->second.rec.height);
+		cairo_fill(cr);
 	}
 	else{
 	}
 	
 }
 for(int i=0; i<topleg.size();i++){
-	if(topleg[i].first.x<width &&topleg[i].first.y<height){
+	if(limitwindowx<=topleg[i].first.x<width+limitwindowx && limitwindowy<=topleg[i].first.y<height+limitwindowy){
 		cairo_set_source_rgb(cr,0,255,0);
 		cairo_rectangle(cr,topleg[i].first.x,topleg[i].first.y,topleg[i].first.width,topleg[i].first.height);
 		cairo_fill(cr);	
 	}
+	else if(limitwindowx<=topleg[i].first.x+topleg[i].first.width<width+limitwindowx && limitwindowy<=topleg[i].first.y<height+limitwindowy){
+		cairo_set_source_rgb(cr,0,255,0);
+		cairo_rectangle(cr,topleg[i].first.x,topleg[i].first.y,topleg[i].first.width,topleg[i].first.height);
+		cairo_fill(cr);
+	}	
 	else{
 //		break;
 	}
 }
 for(int i=0; i<tailleg.size();i++){
-	if(tailleg[i].first.x<width &&tailleg[i].first.y<height){
+	if(limitwindowx<=tailleg[i].first.x<width+limitwindowx && limitwindowy<=tailleg[i].first.y<height+limitwindowy){
+		cairo_set_source_rgb(cr,0,0,255);
+		cairo_rectangle(cr,tailleg[i].first.x,tailleg[i].first.y,tailleg[i].first.width,tailleg[i].first.height);
+		cairo_fill(cr);	
+	}
+	else if(limitwindowx<=tailleg[i].first.x+tailleg[i].first.width<width+limitwindowx && limitwindowy<=tailleg[i].first.y<height+limitwindowy){
 		cairo_set_source_rgb(cr,0,0,255);
 		cairo_rectangle(cr,tailleg[i].first.x,tailleg[i].first.y,tailleg[i].first.width,tailleg[i].first.height);
 		cairo_fill(cr);	
@@ -183,9 +226,125 @@ gboolean deal_key_press(GtkWidget *widget, GdkEventKey  *event, gpointer data)
 	printf("gdk_pixbuf screenshot over\n");
 	}	
     return TRUE;  
-} 
+}
+void scalePinTrackAndLeg(gint x, gint y,gboolean bigorsmall){
+	if(bigorsmall)
+		scalesize*=1.1;
+	else
+		scalesize/=1.1;
+	fontsize*=scalesize;
+	for (int i=0;i<topwnode.size();i++){
+		topwnode[i].rec.x-=x;
+		topwnode[i].rec.y-=y;
+		topwnode[i].rec.x*=scalesize;
+		topwnode[i].rec.y*=scalesize;
+		topwnode[i].rec.x+=x;
+		topwnode[i].rec.y+=y;
+		topwnode[i].rec.width*=scalesize;
+		topwnode[i].rec.height*=scalesize;
+		topPnum[i].x-=x;
+		topPnum[i].y-=y;
+		topPnum[i].x*=scalesize;
+		topPnum[i].y*=scalesize;
+		topPnum[i].x+=x;
+		topPnum[i].y+=y;
+		/////
+		tailwnode[i].rec.x-=x;
+		tailwnode[i].rec.y-=y;
+		tailwnode[i].rec.x*=scalesize;
+		tailwnode[i].rec.y*=scalesize;
+		tailwnode[i].rec.x+=x;
+		tailwnode[i].rec.y+=y;
+		tailwnode[i].rec.width*=scalesize;
+		tailwnode[i].rec.height*=scalesize;
+		tailPnum[i].x-=x;
+		tailPnum[i].y-=y;
+		tailPnum[i].x*=scalesize;
+		tailPnum[i].y*=scalesize;
+		tailPnum[i].x+=x;
+		tailPnum[i].y+=y;
+	}
+	std::map<std::string,Windownode>::iterator ittrack;
+	for(ittrack = outnode.begin(); ittrack != outnode.end(); ittrack++){
+		ittrack->second.rec.x -=x;
+		ittrack->second.rec.y -=y;
+		ittrack->second.rec.x*=scalesize;
+		ittrack->second.rec.y*=scalesize;
+		ittrack->second.rec.x+=x;
+		ittrack->second.rec.y+=y;
+		ittrack->second.rec.width*=scalesize;	
+		ittrack->second.rec.height*=scalesize;
+		
+	}
+	////
+	for(int i=0; i<topleg.size();i++){
+		topleg[i].first.x -=x;
+		topleg[i].first.y -=y;
+		topleg[i].first.x*=scalesize;
+		topleg[i].first.y*=scalesize;
+		topleg[i].first.x+=x;
+		topleg[i].first.y+=y;
+		topleg[i].first.width*=scalesize;
+		topleg[i].first.height*=scalesize;
+		///
+		tailleg[i].first.x -=x;
+		tailleg[i].first.y -=y;
+		tailleg[i].first.x*=scalesize;
+		tailleg[i].first.y*=scalesize;
+		tailleg[i].first.x+=x;
+		tailleg[i].first.y+=y;
+		tailleg[i].first.width*=scalesize;
+		tailleg[i].first.height*=scalesize;
+	}
+	
+}
+gboolean deal_mouse_press(GtkWidget *widget, GdkEventButton *event, gpointer data)
+{
+				gint i ;
+				gint j ;
+	switch(event->button){	// 判斷滑鼠點選的型別
+		case 1:
+			//printf("Left Button!!\n");
+			std::cout<<scalesize<<std::endl;
+			if(event->type == GDK_2BUTTON_PRESS){
+				 i = event->x;
+				 j = event->y;
+				scalePinTrackAndLeg(i,j,false);
+				gtk_widget_queue_draw(window);
+				printf("縮小\n");
+			}
+			break;
+		case 2:
+			break;
+		case 3://右鍵
+
+			std::cout<<scalesize<<std::endl;
+			if(event->type == GDK_2BUTTON_PRESS){
+				 i = event->x;
+				 j = event->y;
+				scalePinTrackAndLeg(i,j,true);
+				gtk_widget_queue_draw(window);
+				printf("放大\n");
+			}
+			break;
+		default:
+			printf("Unknown Button!!\n");
+	}
+
+
+
+	return TRUE;
+}
+
+// 滑鼠移動
+gboolean deal_motion_notify_event(GtkWidget *widget, GdkEventMotion *event, gpointer data)
+{
+	// 獲得移動滑鼠的座標值，距離視窗左頂點
+
+	return TRUE;
+}
 void drawgui(int argc, char * argv[]){
-	GtkWidget *window;
+//	GtkWidget *window;
 
     gtk_init(&argc, &argv);
 
@@ -202,6 +361,11 @@ void drawgui(int argc, char * argv[]){
 	//GDK_KEY_PRESS_MASK 鍵盤按住事件
 	gtk_widget_add_events(window, GDK_BUTTON_PRESS_MASK | GDK_BUTTON_MOTION_MASK|GDK_BUTTON_RELEASE_MASK|GDK_KEY_PRESS_MASK);	
     g_signal_connect(window, "key-press-event",G_CALLBACK(deal_key_press), NULL); //鍵盤事件
+	// "button-press-event" 與 deal_mouse_event 連線，滑鼠點選事件
+	g_signal_connect(window, "button-press-event", G_CALLBACK(deal_mouse_press), NULL);
+	// "motion-notify-event" 與 deal_motion_notify_event 連線，按住滑鼠移動事件
+	g_signal_connect(window, "motion-notify-event", G_CALLBACK(deal_motion_notify_event), NULL);
+
 	
 	gtk_widget_set_app_paintable(window, TRUE);
     gtk_widget_show_all(window);
